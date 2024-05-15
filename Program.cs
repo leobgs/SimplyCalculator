a7773d50c8541302a4b0ca72c16c9ed5b9716983
@leonardo <leonardobagus093@gmail.com>
2024-05-15 13:46:58 +0700

    [CREATE]-operational of calculate

--- Program.cs
+++ Program.cs
@@ -3,9 +3,44 @@ System.Console.WriteLine($"Hello, Welcome!!!");
 
 // make user input.
 System.Console.WriteLine($"Input the first number: ");
-var firstNumber = Console.ReadLine();
+string inputNumber1 = Console.ReadLine();
+int firstNumber = int.Parse(inputNumber1);
 
 System.Console.WriteLine($"Input the second number: ");
-var secondNumber = Console.ReadLine();
+string inputNumber2 = Console.ReadLine();
+int secondNumber = int.Parse(inputNumber2);
 
+// create view user choice
+System.Console.WriteLine($"What do you want to do with those numbers?");
+System.Console.WriteLine($"[A]dd");
+System.Console.WriteLine($"[S]ubract");
+System.Console.WriteLine($"[M]ultiply");
+System.Console.WriteLine($"[D]ivide");
+string selectedOption = Console.ReadLine().ToLower();
 
+// create logical selected option
+int result;
+if (selectedOption == "")
+{
+    System.Console.WriteLine("Invalid input");
+}
+else if (selectedOption == "a")
+{
+    result = firstNumber + secondNumber;
+    System.Console.WriteLine(result);
+}
+else if (selectedOption == "s")
+{
+    result = firstNumber - secondNumber;
+    System.Console.WriteLine(result);
+}
+else if (selectedOption == "m")
+{
+    result = firstNumber * secondNumber;
+    System.Console.WriteLine(result);
+}
+else if (selectedOption == "d")
+{
+    result = firstNumber / secondNumber;
+    System.Console.WriteLine(result);
+}
