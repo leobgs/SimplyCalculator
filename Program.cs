c35a0511f39185f3f00ba826ed96b97f6e43611e
@leonardo <leonardobagus093@gmail.com>
2024-05-15 13:29:50 +0700

    [CREATE]-view choice user

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
-﻿System.Console.WriteLine($"Hello, Welcome!!!");
+﻿// make welcome text.
+System.Console.WriteLine($"Hello, Welcome!!!");
+
+// make user input.
+System.Console.WriteLine($"Input the first number: ");
+var firstNumber = Console.ReadLine();
+
+System.Console.WriteLine($"Input the second number: ");
+var secondNumber = Console.ReadLine();
+
 
