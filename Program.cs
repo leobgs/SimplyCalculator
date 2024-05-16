3c9b64180fb6affbb506a6c3e7c3141906d57c72
@leonardo <leonardobagus093@gmail.com>
2024-05-16 10:39:43 +0700

    [CREATE]-Method for matematical operation and print equation.

--- Program.cs
+++ Program.cs
@@ -1,46 +1,74 @@
-﻿// make welcome text.
-System.Console.WriteLine($"Hello, Welcome!!!");
+﻿bool isRunningProgram = false;
+while (!isRunningProgram)
+{
+    // make welcome text.
+    System.Console.WriteLine($"Hello, Welcome!!!");
 
-// make user input.
-System.Console.WriteLine($"Input the first number: ");
-string inputNumber1 = Console.ReadLine();
-int firstNumber = int.Parse(inputNumber1);
+    // make user input.
+    System.Console.Write($"Input the first number: ");
+    string inputNumber1 = Console.ReadLine();
+    int firstNumber = int.Parse(inputNumber1);
 
-System.Console.WriteLine($"Input the second number: ");
-string inputNumber2 = Console.ReadLine();
-int secondNumber = int.Parse(inputNumber2);
+    System.Console.Write($"Input the second number: ");
+    string inputNumber2 = Console.ReadLine();
+    int secondNumber = int.Parse(inputNumber2);
 
-// create view user choice
-System.Console.WriteLine($"What do you want to do with those numbers?");
-System.Console.WriteLine($"[A]dd");
-System.Console.WriteLine($"[S]ubract");
-System.Console.WriteLine($"[M]ultiply");
-System.Console.WriteLine($"[D]ivide");
-string selectedOption = Console.ReadLine().ToLower();
+    // create view user choice
+    System.Console.WriteLine($"What do you want to do with those numbers?");
+    System.Console.WriteLine($"[A]dd");
+    System.Console.WriteLine($"[S]ubract");
+    System.Console.WriteLine($"[M]ultiply");
+    System.Console.WriteLine($"[D]ivide");
+    System.Console.WriteLine($"[E]xit");
+    string selectedOption = Console.ReadLine().ToLower();
 
-// create logical selected option
-int result;
-if (selectedOption == "")
-{
-    System.Console.WriteLine("Invalid input");
+    // create logical selected option
+    int result;
+    if (selectedOption == "")
+    {
+        System.Console.WriteLine("Invalid input");
+    }
+    else if (selectedOption == "a")
+    {
+        result = AddEquation(firstNumber, secondNumber);
+        PrintTextEquation(firstNumber, "+", secondNumber, result);
+    }
+    else if (selectedOption == "s")
+    {
+        result = SubtractEquation(firstNumber, secondNumber);
+        PrintTextEquation(firstNumber, "-", secondNumber, result);
+    }
+    else if (selectedOption == "m")
+    {
+        result = MultiplyEquation(firstNumber, secondNumber);
+        PrintTextEquation(firstNumber, "*", secondNumber, result);
+    }
+    else if (selectedOption == "d")
+    {
+        result = DivideEquation(firstNumber, secondNumber);
+        PrintTextEquation(firstNumber, "/", secondNumber, result);
+    }
+    else if (selectedOption == "e")
+    {
+        System.Console.WriteLine($"Want to Exit? Good Bye :)");
+        isRunningProgram = true;
+    }
+    else
+    {
+        System.Console.WriteLine("${Invalid Input}");
+    }
+    System.Console.WriteLine("Press any key to continue previous action");
+    Console.ReadKey();
 }
-else if (selectedOption == "a")
+
+// create method equation
+static int AddEquation(int number1, int number2) => number1 + number2;
+static int SubtractEquation(int number1, int number2) => number1 - number2;
+static int MultiplyEquation(int number1, int number2) => number1 * number2;
+static int DivideEquation(int number1, int number2) => number1 / number2;
+
+// create method print equation
+void PrintTextEquation(int number1, string @operator, int number2, int result1)
 {
-    result = firstNumber + secondNumber;
-    System.Console.WriteLine(result);
-}
-else if (selectedOption == "s")
-{
-    result = firstNumber - secondNumber;
-    System.Console.WriteLine(result);
-}
-else if (selectedOption == "m")
-{
-    result = firstNumber * secondNumber;
-    System.Console.WriteLine(result);
-}
-else if (selectedOption == "d")
-{
-    result = firstNumber / secondNumber;
-    System.Console.WriteLine(result);
+    System.Console.WriteLine($"The result {number1} {@operator} {number2} = {result1}");
 }
